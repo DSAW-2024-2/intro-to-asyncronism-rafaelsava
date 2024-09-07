71475f6c5cea20f4b3198e0f8a34b890eac69220
Rafael Salcedo <rafaelsava@unisabana.edu.co>
2024-09-07 14:57:54 -0500

    errors management

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
 const pokemonGrid = document.querySelector('.pokemon-grid');
-const main =document.querySelector('main');
+const main = document.querySelector('main');
 const typeFilter = document.getElementById('type-filter');
 const searchInput = document.getElementById('search');
-const searchBtn = document.getElementById('search-btn');
 const modal = document.getElementById('pokemon-modal');
+const btnSearch = document.getElementById('search-btn');
+const imgPokedex = document.querySelector('.imgPokeName');
 
 const URLGeneral = 'https://pokeapi.co/api/v2/pokemon/';
 const URLType = 'https://pokeapi.co/api/v2/type/';
-const URLSpecies = 'https://pokeapi.co/api/v2/pokemon-species/'
+const URLSpecies = 'https://pokeapi.co/api/v2/pokemon-species/';
 
 let start = 0;
 let limit = 20;
 
-async function loadPokelinks(){
-    const response = await fetch(`${URLGeneral}?offset=${start}&limit=${limit}`);
-    const data = await response.json();
-    
-    // Promises to load pokemons
-    const pokemonPromises = data.results.map(pokemon => loadPokemon(pokemon.url));
-    
-    //Wait to all promises to be solved
-    await Promise.all(pokemonPromises);
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 
-    // Add button after all promises
-    addButtonMorePokemons();
+async function loadPokelinks() {
+    const data = await fetchData(`${URLGeneral}?offset=${start}&limit=${limit}`);
+    if (data) {
+        const pokemonPromises = data.results.map(pokemon => loadPokemon(pokemon.url));
+        await Promise.all(pokemonPromises);
+        addButtonMorePokemons();
+    }
 }
 
 async function loadPokemon(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    showPokemon(data);
+    let loader = document.querySelector('.loader');
+    
+    if (!loader) {
+        loader = document.createElement('div');
+        loader.classList.add('loader');
+        pokemonGrid.append(loader);  
+    }
+
+    loader.style.display = 'block'; 
+
+    const data = await fetchData(url);
+    if (data) {
+        showPokemon(data);
+        loader.style.display = 'none'; 
+        return true; 
+    } else {
+        pokemonGrid.innerHTML = '<p class="no-pokemon">This pokemon doesn\'t exist.<br>Click on pokedex image to come back.</p>';
+        loader.style.display = 'none'; 
+        return false; 
+    } 
 }
 
 function addButtonMorePokemons() {
+    const existingButton = document.querySelector('.load-more-btn');
+    if (existingButton) existingButton.remove();
+
     const morePoke = document.createElement('button');
     morePoke.textContent = 'Load More';
     morePoke.classList.add('load-more-btn');
@@ -40,23 +68,14 @@ function addButtonMorePokemons() {
 
     morePoke.addEventListener('click', () => {
         morePoke.remove(); 
-        start += limit;  
-        loadPokelinks();  
+        start += limit;
+        loadPokelinks();
     });
-
 }
 
-// Function to show pokemons
 function showPokemon(data) {
-    let tipos = data.types.map(type => type.type.name);
-    if (tipos.length === 1) {
-        tipos = `<p class="type-${tipos[0]} type">${tipos[0]}</p>`;
-    } else {
-        tipos = `
-            <p class="type-${tipos[0]} type">${tipos[0]}</p>
-            <p class="type-${tipos[1]} type">${tipos[1]}</p>`;
-    }
-
+    const tipos = data.types.map(type => `<p class="type-${type.type.name} type">${type.type.name}</p>`).join('');
+    
     const div = document.createElement('div');
     div.classList.add('pokemon-card');
     div.innerHTML = `
@@ -70,36 +89,33 @@ function showPokemon(data) {
             <p class="stat">${data.weight}kg</p>
         </div>`;
     
-    div.addEventListener('click', () => {
-        loadPokemonSpecies(data.id);
-    });
-
+    div.addEventListener('click', () => loadPokemonSpecies(data.id));
     pokemonGrid.append(div);
 }
 
-// Function to filter pokemons by type or search them
 async function filterPokemons() {
     const selectedType = typeFilter.value;
-    const searchText = searchInput.value.toLowerCase(); 
-    pokemonGrid.innerHTML = '';
+    const searchText = searchInput.value.toLowerCase();
+    const reloadMessage = document.querySelector('.reload-message');
+    if (reloadMessage) {
+        reloadMessage.remove();
+    }
 
-    if (selectedType !== '' && searchText === '') {
-        const response = await fetch(`${URLType}${selectedType}`);
-        const data = await response.json();
-        data.pokemon.forEach(pokemonEntry => {
-            const pokemonData = pokemonEntry.pokemon;
-            loadPokemon(pokemonData.url);
-        });
-    } else if (searchText !== '') {
-        loadPokemon(`${URLGeneral}${searchText}`);
-    } else {
-        start = 0; 
+    pokemonGrid.innerHTML = '';
+    const existingButton = document.querySelector('.load-more-btn');
+    if (existingButton) existingButton.remove();
+
+    if (selectedType && !searchText) {
+        const data = await fetchData(`${URLType}${selectedType}`);
+        if (data) {
+            const promises = data.pokemon.map(pokemonEntry => loadPokemon(pokemonEntry.pokemon.url));
+            await Promise.all(promises);
+        }
+    } else if(!selectedType && !searchText){
+        start = 0;
         loadPokelinks();
     }
-    const button = document.querySelector('.load-more-btn')
-    button.remove();
 }
-
 
 function showModal() {
     modal.style.display = 'block';
@@ -111,21 +127,20 @@ window.addEventListener('click', (event) => {
     }
 });
 
-//Load data from species endpoint
-async function loadPokemonSpecies(id){
-    const response = await fetch(`${URLSpecies}${id}`);
-    const data = await response.json();
-    showPokemonSpecies(data,id)
+async function loadPokemonSpecies(id) {
+    const data = await fetchData(`${URLSpecies}${id}`);
+    if (data) {
+        showPokemonSpecies(data, id);
+    }
 }
 
+function showPokemonSpecies(data, id) {
+    let description = data.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text;
+    description = description.replace(/\u000c/g, ' ');
 
-//show modal with info from species endpoint
-async function showPokemonSpecies(data,id) {
-    const description = data.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text;
     const habitat = data.habitat ? data.habitat.name : 'Unknown';
     const generation = data.generation.name;
 
-    // Actualizar el innerHTML del modal
     modal.innerHTML = `
         <div class="modal-content">
             <span class="close">&times;</span>
@@ -134,19 +149,55 @@ async function showPokemonSpecies(data,id) {
             <p>${description}</p>
             <p><b>Habitat</b>: ${habitat}</p>
             <p><b>Generation</b>: ${generation}</p>
-
         </div>`;
 
     showModal();
+
     const closeModalBtn = modal.querySelector('.close');
     closeModalBtn.addEventListener('click', () => {
         modal.style.display = 'none';
     });
+}
 
+async function handlePokemonSearch() {
+    const searchText = searchInput.value.trim().toLowerCase();
+    const validInput = /^[a-z0-9]+$/i;
+
+    if (validInput.test(searchText)) {
+        const success = await loadPokemon(`${URLGeneral}${searchText}`);
+        if(success){
+            const message = document.createElement('p');
+            message.classList.add('reload-message');
+            message.textContent = 'Click on Pokedex image to go back.';
+            main.append(message);
+        }
+        searchInput.value = '';
+    } else {
+        window.alert('Please enter a valid Pokemon name (letters and numbers only).');
+    }
 }
 
 typeFilter.addEventListener('change', filterPokemons);
-searchBtn.addEventListener('click', filterPokemons);
 searchInput.addEventListener('input', filterPokemons);
+btnSearch.addEventListener('click', handlePokemonSearch);
+searchInput.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
+        handlePokemonSearch();
+    }
+});
+
+
+
+imgPokedex.addEventListener('click', () => {
+    typeFilter.value = '';
+    searchInput.value = '';
+    pokemonGrid.innerHTML = '';
+    start = 0;
+    loadPokelinks();
+    const reloadMessage = document.querySelector('.reload-message');
+    if (reloadMessage) {
+        reloadMessage.remove();
+    }
+});
 
 loadPokelinks();
